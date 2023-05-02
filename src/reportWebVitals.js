2e68ec6a831ecbe6d328f7fafe26311e160e38af
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-05-02 20:36:58 +0500

    update changes

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,15 +1,12 @@
 const onPerfEntry = () => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({
-       getCLS, getFID, getFCP, getLCP, getTTFB 
-      }) => {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
-      getFID(onPerfEntry);
+    getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
     });
   }
 };
-
-export default reportWebVitals;
+export default onPerfEntry;
