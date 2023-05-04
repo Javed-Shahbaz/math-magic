cae2c9878e82fc2c88c0f933b4c1de7e8c30a0e2
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-05-04 06:28:14 +0500

    update changes

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,7 +1,79 @@
+import React from 'react';
+
 function Calc() {
   return (
-    <div>0</div>
+    <div className="main">
+      <div className="screen">0</div>
+      <div className="buttons">
+        <div className="lines">
+          <button type="button" className="grey">
+            Ac
+          </button>
+          <button type="button" className="grey">
+            +/-
+          </button>
+          <button type="button" className="grey">
+            %
+          </button>
+          <button type="button" className="orangBtn grey">
+            ÷
+          </button>
+        </div>
+        <div className="lines">
+          <button type="button" className="grey">
+            7
+          </button>
+          <button type="button" className="grey">
+            8
+          </button>
+          <button type="button" className="grey">
+            9
+          </button>
+          <button type="button" className="orangBtn grey">
+            X
+          </button>
+        </div>
+        <div className="lines">
+          <button type="button" className="grey">
+            4
+          </button>
+          <button type="button" className="grey">
+            5
+          </button>
+          <button type="button" className="grey">
+            6
+          </button>
+          <button type="button" className="orangBtn grey">
+            -
+          </button>
+        </div>
+        <div className="lines">
+          <button type="button" className="grey">
+            1
+          </button>
+          <button type="button" className="grey">
+            2
+          </button>
+          <button type="button" className="grey">
+            3
+          </button>
+          <button type="button" className="orangBtn grey">
+            +
+          </button>
+        </div>
+        <div className="lines">
+          <button type="button" className="zero grey">
+            0
+          </button>
+          <button type="button" className="grey">
+            .
+          </button>
+          <button type="button" className="orangBtn grey">
+            =
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
-
 export default Calc;
