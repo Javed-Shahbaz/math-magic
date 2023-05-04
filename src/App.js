ee5c57a5902f6b074b9a7a9374d6ba953a429f1c
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-05-04 18:04:28 +0500

    update changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { render } from '@testing-library/react';
-import Calc from './components/calculator.js';
+import Calc from './components/Calculator.js';
 
 function App() {
   return (
     <Calc />
-      
   );
 }
 
