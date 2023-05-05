d42017a31be2cd926c3edd35429787711c200770
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-05-05 17:20:38 +0500

    update changes

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -8,41 +8,41 @@ function Calc() {
   };
   return (
     <>
-      <div className="main">
-        <div className="screen">
+      <div className='main'>
+        <div className='screen'>
           {presentState.total}
           {presentState.operation}
           {presentState.next}
         </div>
-        <div className="buttons">
+        <div className='buttons'>
           <div className='lines'>
-          <button type="button" onClick={mouseClick} className="grey">AC</button>
-          <button type="button" onClick={mouseClick} className="grey">+/-</button>
-          <button type="button" onClick={mouseClick} className="grey">%</button>
-          <button type="button" onClick={mouseClick} className="orangBtn grey">÷</button>
+            <button type='button' onClick={mouseClick} className='grey'>AC</button>
+            <button type='button' onClick={mouseClick} className='grey'>+/-</button>
+            <button type='button' onClick={mouseClick} className='grey'>%</button>
+            <button type='button' onClick={mouseClick} className='orangBtn grey'>÷</button>
           </div>
           <div className='lines'>
-          <button type="button" onClick={mouseClick} className="grey">7</button>
-          <button type="button" onClick={mouseClick} className="grey">8</button>
-          <button type="button" onClick={mouseClick} className="grey">9</button>
-          <button type="button" onClick={mouseClick} className="orangBtn grey">x</button>
+            <button type='button' onClick={mouseClick} className='grey'>7</button>
+            <button type='button' onClick={mouseClick} className='grey'>8</button>
+            <button type='button' onClick={mouseClick} className='grey'>9</button>
+            <button type='button' onClick={mouseClick} className='orangBtn grey'>x</button>
           </div>
           <div className='lines'>
-          <button type="button" onClick={mouseClick} className="grey">4</button>
-          <button type="button" onClick={mouseClick} className="grey">5</button>
-          <button type="button" onClick={mouseClick} className="grey">6</button>
-          <button type="button" onClick={mouseClick} className="orangBtn grey">-</button>
+            <button type='button' onClick={mouseClick} className='grey'>4</button>
+            <button type='button' onClick={mouseClick} className='grey'>5</button>
+            <button type='button' onClick={mouseClick} className='grey'>6</button>
+            <button type='button' onClick={mouseClick} className='orangBtn grey'>-</button>
+          </div>
+            <div className='lines'>
+            <button type='button' onClick={mouseClick} className='grey'>1</button>
+            <button type='button' onClick={mouseClick} className='grey'>2</button>
+            <button type='button' onClick={mouseClick} className='grey'>3</button>
+            <button type='button' onClick={mouseClick} className='orangBtn grey'>+</button>
           </div>
           <div className='lines'>
-          <button type="button" onClick={mouseClick} className="grey">1</button>
-          <button type="button" onClick={mouseClick} className="grey">2</button>
-          <button type="button" onClick={mouseClick} className="grey">3</button>
-          <button type="button" onClick={mouseClick} className="orangBtn grey">+</button>
-          </div>
-          <div className='lines'>
-          <button type="button" onClick={mouseClick} className="zero grey">0</button>
-          <button type="button" onClick={mouseClick} className="grey">.</button>
-          <button type="button" onClick={mouseClick} className="orangBtn grey">=</button>
+            <button type='button' onClick={mouseClick} className='zero grey'>0</button>
+            <button type='button' onClick={mouseClick} className='grey'>.</button>
+            <button type='button' onClick={mouseClick} className='orangBtn grey'>=</button>
           </div>
         </div>
       </div>
