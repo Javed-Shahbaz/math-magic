3157335768cbec562a473d07f978ef3db9b4dbe8
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-05-12 15:38:34 +0500

    update files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import './index.css';
 import Calculator from './components/Calculator';
-import Quote from './components/Quote';
+import Quot from './components/Quotes';
 import Home from './components/Home';
 import Layout from './components/Layout';
 
@@ -14,7 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />}> </Route>
         <Route path="calculator" element={<Calculator />}> </Route>
-        <Route path="quote" element={<Quote />} />
+        <Route path="quote" element={<Quot />} />
       </Routes>
     </>
   );
