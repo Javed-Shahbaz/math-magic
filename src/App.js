265444f1214a95d55d388c396be95616fe650b3f
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-05-12 15:18:37 +0500

    update scripts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import Calc from './components/calculator';
-import Quote from './components/Quotes';
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Calculator from './components/Calculator';
+import Quote from './components/Quote';
+import Home from './components/Home';
+import Layout from './components/Layout';
 
-function App() {
+const App = () => {
   return (
     <>
-      <Calc />
-      <Quote />
+      <Layout />
+      <Routes>
+        <Route path="/" element={<Home />}> </Route>
+        <Route path="calculator" element={<Calculator />}> </Route>
+        <Route path="quote" element={<Quote />} />
+      </Routes>
     </>
   );
 }
